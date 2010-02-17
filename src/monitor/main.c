b77570ca5ff3f9589f354d40083cc8f40b716333
Holger Hetterich <hhetter@novell.com>
2010-02-17 23:14:02 +0100

    Added basic skeleton based on cmake
    Added GPLv3 COPYING, AUTHOR File, and to dummy
    c files for testing cmake.

---
+++ src/monitor/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("... doesn't mean you've got the right.\n");
+}
---
+++ src/tool/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("Just'cause you've got the power...\n");
+}
