32fb6e22d647c978a53e17cc875aa06b34f771f7
Michael Haefner <mhaefner@suse.de>
2010-02-24 09:49:03 +0100

    added iniparser3.0b; CMakeLists.txt: added check for iniparser main.c: added basic ncurses structure

--- src/monitor/main.c
+++ src/monitor/main.c
@@ -18,9 +18,57 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+
+#define VERSION "0.0.1"
+
+WINDOW *win;
+
+void monitor_shutdown(void)
+{
+	endwin();
+	system("clear");
+	curs_set(1);
+	fprintf(stdout, "smbtamonitor %s by Michael Haefner\n", VERSION);
+	exit(0);
+}
 
 void main(void)
 {
-	printf("... doesn't mean you've got the right.\n");
-}
+	short int key_pressed;
+	
+	initscr();
+	atexit(monitor_shutdown);
+	clear();
+	noecho();
+	curs_set(0);
+	cbreak();
+	keypad(stdscr, 1);
+	
+	start_color();
+	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+
+	win = newwin(5, 20, 3, 1);
+
+	bkgd(COLOR_PAIR(1));
+	wbkgd(win, COLOR_PAIR(1));
+
+	mvaddstr(1,25, "smbtamonitor v0.1");
+	mvwaddstr(win, 1, 1, "Overall statistics");
+
+	box(win, 0, 0);
+	mvhline(2, 25, ACS_BULLET, 17);
+
+	refresh();
+	wrefresh(win);
+
+	key_pressed = getch();
+
+	while(key_pressed != 'q')
+	{
+	}
+
+	return;
+
+}	
