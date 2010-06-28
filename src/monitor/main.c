9d0fede515c5277f497290dd9c464d090caae5f9
Holger Hetterich <hhetter@novell.com>
2010-06-28 12:28:00 +0200

    In preparation for networking code, remove some of the curses stuff

--- src/monitor/main.c
+++ src/monitor/main.c
@@ -36,39 +36,5 @@ void monitor_shutdown(void)
 
 int main(void)
 {
-	short int key_pressed;
-	
-	initscr();
-	atexit(monitor_shutdown);
-	clear();
-	noecho();
-	curs_set(0);
-	cbreak();
-	keypad(stdscr, 1);
-	
-	start_color();
-	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-
-	win = newwin(5, 20, 3, 1);
-
-	bkgd(COLOR_PAIR(1));
-	wbkgd(win, COLOR_PAIR(1));
-
-	mvaddstr(1,25, "smbtamonitor v0.1");
-	mvwaddstr(win, 1, 1, "Overall statistics");
-
-	box(win, 0, 0);
-	mvhline(2, 25, ACS_BULLET, 17);
-
-	refresh();
-	wrefresh(win);
-
-	key_pressed = getch();
-
-	while(key_pressed != 'q')
-	{
-	}
-
-	return 0;
 
 }	
