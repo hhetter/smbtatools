8388aedefa4b5f28cfd8d35226d7a3317ec9c01c
Holger Hetterich <hhetter@novell.com>
2010-10-25 19:33:47 +0200

    all source defined versions to "1.0"

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
-#define SMBTAQUERY_VERSION "0.1"
-#define SMBTAMONITOR_VERSION "0.1"
-#define SMBTATORTURE_VERSION "0.1"
+#define SMBTAQUERY_VERSION "1.0"
+#define SMBTAMONITOR_VERSION "1.0"
+#define SMBTATORTURE_VERSION "1.0"
 
 #define SMBTA_LICENSE "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
 
