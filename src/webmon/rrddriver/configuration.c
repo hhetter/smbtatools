3c081385e5d374b3b2eba4412cd2f18d2e3bb350
Holger Hetterich <hhetter@novell.com>
2010-11-02 12:19:59 +0100

    [WEBMON] add a check for "/usr/bin/rrdtool" to rrddriver

--- src/webmon/rrddriver/configuration.c
+++ src/webmon/rrddriver/configuration.c
@@ -241,6 +241,7 @@ int configuration_parse_cmdline( struct configuration_data *c,
 
 int configuration_check_configuration( struct configuration_data *c )
 {
+	FILE *rrdtoolbin;
         if ( c->debug_level <0 || c->debug_level>10 ) {
                 printf("ERROR: debug level has to be between 0 and 10.\n");
                 return -1;
@@ -251,6 +252,14 @@ int configuration_check_configuration( struct configuration_data *c )
         }
 	if (c->timer <= 0 ) {
 		printf("ERROR: timer for the update intervall is too short!\n");
+		return -1;
+	}
+	/* check for rrdtool to be available */
+	rrdtoolbin = fopen("/usr/bin/rrdtool","r");
+	if (rrdtoolbin != NULL)
+		fclose(rrdtoolbin);
+	else {
+		printf("ERROR: rrdtool doesn't exist in /usr/bin/rrdtool !\n");
 		return -1;
 	}
         return 0;
