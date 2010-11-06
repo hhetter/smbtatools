3ac803f8bbbe2061f3abda3e90be3d5fba2b9eef
Holger Hetterich <hhetter@novell.com>
2010-11-06 21:08:57 +0100

    Bump to version 1.1
    Conflicts:
    
    	include/version.h

--- include/version.h
+++
@@ -1,6 +0,0 @@
-#define SMBTAQUERY_VERSION "1.0"
-#define SMBTAMONITOR_VERSION "1.0"
-#define SMBTATORTURE_VERSION "1.0"
-#define RRDDRIVER_VERSION "1.0"
-#define SMBTA_LICENSE "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
-
