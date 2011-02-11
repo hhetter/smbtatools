6574dac676c95e84760736e4599b3f2ef1b48d67
Benjamin Brunner <bbrunner@suse.de>
2011-02-11 21:19:13 +0100

    [query][webui]Dynamic configuration integrated

--- src/query/webui/app/controllers/list_controller.rb
+++ src/query/webui/app/controllers/list_controller.rb
@@ -2,7 +2,10 @@ class ListController < ApplicationController
   require "rexml/document"
   include REXML
   def index
-    @test=`/home/benni/run/smbtaquery -h 10.10.103.116 -i 3491 -q 'global, list shares;' -x /home/benni/run/list.xml`
+debugger
+    cmd="/home/benni/run/smbtaquery -h " + $host_ip + " -i " + $port + " -q 'global, list shares;' -x /home/benni/run/list.xml"
+    `#{cmd}`
+#    `/home/benni/run/smbtaquery -h 10.10.103.116 -i 3491 -q 'global, list shares;' -x /home/benni/run/list.xml`
     @shares = Array.new
     file = File.new( "/home/benni/run/list.xml" )
     doc = Document.new file
