ac0cc0ddf0de9400126fceb5a465eed7f663ccf4
Benjamin Brunner <bbrunner@suse.de>
2011-02-11 15:42:14 +0100

    parse xml files

--- src/query/webui/app/controllers/list_controller.rb
+++ src/query/webui/app/controllers/list_controller.rb
@@ -1,5 +1,13 @@
 class ListController < ApplicationController
+  require "rexml/document"
+  include REXML
   def index
+    @shares = Array.new
+    file = File.new( "/home/benni/run/fritz.xml" )
+    doc = Document.new file
+    doc.elements.each("smbta_output/list/share") {
+      |e| @shares << e.text 
+    }
   end
 
 end
