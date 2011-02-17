1d0d810e4494e01c6f6bbd412cb5157ec967d7a8
Benjamin Brunner <bbrunner@suse.de>
2011-02-17 11:30:38 +0100

    [query][webui]Changed ruby-version from 1.9.x to 1.8.x Changed path to smbtaquery binary and xml-path

--- src/query/webui/app/controllers/list_controller.rb
+++ src/query/webui/app/controllers/list_controller.rb
@@ -2,11 +2,11 @@ class ListController < ApplicationController
   require "rexml/document"
   include REXML
   def index
-    cmd="/home/benni/run/smbtaquery -h " + $host_ip + " -i " + $port + " -q 'global, list shares;' -x /home/benni/run/list.xml"
+    cmd="$HOME/run/smbtaquery -h " + $host_ip + " -i " + $port + " -q 'global, list shares;' -x /tmp/list.xml"
     `#{cmd}`
 #    `/home/benni/run/smbtaquery -h 10.10.103.116 -i 3491 -q 'global, list shares;' -x /home/benni/run/list.xml`
     @shares = Array.new
-    file = File.new( "/home/benni/run/list.xml" )
+    file = File.new( "/tmp/list.xml" )
     doc = Document.new file
     doc.elements.each("smbta_output/list/table_row/table_value[@id='sharename']") {
       |e| @shares << e.text 
