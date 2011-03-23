7b3abb2a279ce80ef357139126536540d38346a2
Holger Hetterich <hhetter@novell.com>
2011-03-23 12:42:35 +0100

    Bump version -> 1.2.3

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
-#define SMBTAQUERY_VERSION "1.2.2"
-#define SMBTAMONITOR_VERSION "1.2.2"
-#define SMBTATORTURE_VERSION "1.2.2"
-#define RRDDRIVER_VERSION "1.2.2"
+#define SMBTAQUERY_VERSION "1.2.3"
+#define SMBTAMONITOR_VERSION "1.2.3"
+#define SMBTATORTURE_VERSION "1.2.3"
+#define RRDDRIVER_VERSION "1.2.3"
 #define SMBTA_LICENSE "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
 
