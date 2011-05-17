cb6ed43e5e67e254811cc9755162305e897980da
Holger Hetterich <hhetter@novell.com>
2011-05-17 13:34:44 +0200

    Raise version 1.2.5

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
-#define SMBTAQUERY_VERSION "1.2.4"
-#define SMBTAMONITOR_VERSION "1.2.4"
-#define SMBTATORTURE_VERSION "1.2.4"
-#define RRDDRIVER_VERSION "1.2.4"
-#define SMBTATORTURESRV_VERSION "1.2.4"
+#define SMBTAQUERY_VERSION "1.2.5"
+#define SMBTAMONITOR_VERSION "1.2.5"
+#define SMBTATORTURE_VERSION "1.2.5"
+#define RRDDRIVER_VERSION "1.2.5"
+#define SMBTATORTURESRV_VERSION "1.2.5"
 #define SMBTA_LICENSE "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>"
 
