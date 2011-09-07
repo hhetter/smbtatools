2bae2bede5200e28074e8d055aff3b01232112b5
Holger Hetterich <hhetter@novell.com>
2011-09-07 17:14:03 +0200

    add graphics.c and header

---
+++ src/query/include/graphics.h
@@ -0,0 +1,44 @@
+/* 
+ * smbtaquery 
+ * capture transfer data from the vfs_smb_traffic_analyzer module, and store
+ * the data via various plugins
+ *
+ * Copyright (C) Holger Hetterich, 2008
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define _XOPEN_SOURCE
+#define _SVID_SOURCE || _BSD_SOURCE || _XOPEN_SOURCE >= 500
+#define __EXTENSIONS__
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <signal.h>
+#include <getopt.h>
+#include <syslog.h>
+#include <sys/select.h>
+#include <dlfcn.h>
+#include <talloc.h>
+#include "../../../iniparser3.0b/src/iniparser.h"
+#include "configuration.h"
+#include <dbi.h>
+
