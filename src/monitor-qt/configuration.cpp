c90de61392133e3619fdcd7494785f90ea91c0aa
Nanuk <nanuk@bsmpc.site>
2011-09-23 23:55:24 +0200

    Added a newer version of the smbtamonitor-qt, that still has no real functionality. For further development I will try to implement a working vizalization first before taking care about a nicer GUI. This one is ugly but will do the job for now. Build is still broken.

--- src/monitor-qt/configuration.cpp
+++ src/monitor-qt/configuration.cpp
@@ -1,5 +1,6 @@
  
-# include "configuration.h"
+
+ # include "configuration.h"
 
 
  Configuration::~Configuration()
@@ -11,10 +12,22 @@
     qDebug() << "bleh";
     remoteconfigbutton = new QPushButton("remotething");
     configwidget = new QWidget;
-    configgridlayout = new QGridLayout(configwidget);
-    message1 = new QLabel("Eins", configwidget);
-    message2 = new QLabel("zweo", configwidget);
-    configgridlayout->addWidget(message1, 1,1);
-    configgridlayout->addWidget(message2, 10,1);
+//    configgridlayout = new QGridLayout(configwidget);
+//    message1 = new QLabel("Eins", configwidget);
+//    message2 = new QLabel("zweo", configwidget);
+    
+    hostline = new QLineEdit("localhost");
+    portline = new QLineEdit("3942");
+    shareline = new QLineEdit("holger");
+    timeline = new QLineEdit("1h");
+ 
+    configform = new QFormLayout(configwidget);
+    configform->addRow("Hostname:", hostline);
+    configform->addRow("Port:", portline);
+    configform->addRow("Share:", shareline);
+    configform->addRow("Timeframe:", timeline);
+    
+//    configgridlayout->addWidget(message1, 1,1);
+//    configgridlayout->addWidget(message2, 10,1);
 
 }
--- src/monitor-qt/smbtamonitor-run.cpp
+++ src/monitor-qt/smbtamonitor-run.cpp
@@ -1,1 +1,23 @@
  
+ 
+
+ # include "smbtamonitor-run.h"
+
+
+  
+ Smbtamonitor_run::~Smbtamonitor_run(){}
+
+ Smbtamonitor_run::Smbtamonitor_run(QWidget *parent)
+{
+  
+  
+ // qDebug()<< "Hurz";
+}
+
+void Smbtamonitor_run::run(){
+  
+ qDebug() << "run, run!";
+ monitorprocess = new QProcess;
+ monitorprocess->start("./owntools");
+ qDebug() << "run another time!";
+}
