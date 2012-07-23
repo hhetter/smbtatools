8534d6a31801fec42bd14e1932816d0c4e41ff6d
Nanuk Krinner <nkrinner@suse.de>
2012-07-23 15:33:23 +0200

    New graph module sceleton

--- src/monitor-qt/graph.cpp
+++ src/monitor-qt/graph.cpp
@@ -5,6 +5,8 @@ Graph::Graph(QWidget *parent) :
 {
         i_s_count = 0;
         i_stepsize=5;
+        i_x_d_size = 400;
+        i_dp_number = i_x_d_size;
 }
 
 
@@ -12,10 +14,68 @@ Graph::Graph(QWidget *parent) :
 void Graph::g_receivelist(QLinkedList<unsigned long> readlist,                           QLinkedList<unsigned long> writelist)
  {
                 qDebug()<<"g_receivelist.readlist.size: "<< readlist.size();
+                g_get_w_size();
 
 
  }
 
+
+/*
+ * Get the size of the widget that contains the graph
+ * Basic implementation done
+ * Widget size doesn't change yet so no visible effect
+ * Method not called yet
+ */
+void Graph::g_get_w_size()
+{
+    QSize w_size = this->frameSize();
+    qDebug()<<"QSize: y " << w_size.height();
+    qDebug()<<"QSize calc: y " << (int)(w_size.height()*0.8);
+    qDebug()<<"QSize: x " << w_size.width();
+}
+
+void Graph::g_get_dp_offset(){} // Get the datapoint offset to t=0
+
+
+
+void Graph::g_change_dp_num() // Change the number of datapoints for the graph
+{
+    if(/* signal to zoom in*/ true)
+    {
+        i_dp_number = (int)(i_dp_number*0.8);
+    }
+
+    if(/* signal to zoom out*/ true)
+    {
+        i_dp_number = (int)(i_dp_number*1.2);
+    }
+}
+
+
+
+void Graph::g_def_dp_num() // Map datapoints to pixels
+{
+    // 1) find out if there are more pixels or more datapoints
+    if(i_dp_number == i_dp_number){}
+    // 2) find a method to map datapoints to pixels
+
+
+}
+
+
+
+void Graph::g_interpolate(){} // Interpolation and create points to make the graph
+
+
+
+
+
+
+
+void Graph::wheelEvent(QWheelEvent *event)
+{
+
+}
 
 
 
--- src/monitor-qt/graph.h
+++ src/monitor-qt/graph.h
@@ -4,6 +4,8 @@
 #include <QWidget>
 #include <QLinkedList>
 #include <QDebug>
+#include <QSize>
+
 
 class Graph : public QWidget
 {
@@ -13,12 +15,26 @@ public:
 
         int i_s_count;
         int i_stepsize;
+        int i_x_d_size; // Stores width of the diagram - fixed for now, will change dynamically in the future
+        int i_y_d_size; // Stores height of the diagram - fixed for now, will change dynamically in the future
+        int i_x_w_size; // Stores width of the widget that holds the diagram - fixed for now, will change dynamically in the future
+        int i_y_w_size; // Stores height of the widget that holdsdiagram - fixed for now, will change dynamically in the future
+        int i_dp_number; // Number of data points to be displayed in the graph - initialla i_dp_number = i_x_d_size
+        int i_dp_start, i_dp_end; // Defines index of the datapoints that will appear on the graph
+
 
 signals:
 
 public slots:
 
         void g_receivelist(QLinkedList<unsigned long> readlist, QLinkedList<unsigned long> writelist);
+        void g_get_w_size(); // Get the size of the widget that contains the graph
+        void g_get_dp_offset(); // Get the datapoint offset to t=0
+        void g_change_dp_num(); // Change the number of datapoints for the graph
+        void g_def_dp_num(); // Map datapoints to pixels
+        void g_interpolate(); // Inteerpolation and create points to make the graph
+
+        void wheelEvent(QWheelEvent *event); // Will catch wheel events to zoom the graph
 
 
 
