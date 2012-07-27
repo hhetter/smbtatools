273774558eafa2632f3ef261b25e8a30bef58497
Holger Hetterich <hhetter@novell.com>
2012-07-27 12:58:01 +0200

    remove warning from build

--- src/monitor-qt/configform.cpp
+++ src/monitor-qt/configform.cpp
@@ -58,7 +58,6 @@ void ConfigForm::cf_readconfig(){
     *configString = "";
 
     //qDebug()<<"Current index: " <<ui->queryTypeButton->currentIndex();
-    int i_switch = ui->queryTypeButton->currentIndex();
     //qDebug()<<"i_switch: " <<i_switch;
 
     switch(ui->queryTypeButton->currentIndex())
